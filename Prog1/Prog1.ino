f36e89794962eeaea704097a0ce0a0443a7beebd
jordipont2019 <jordi20pont@hotmail.com>
2020-02-03 20:33:34 +0100

    Initial commit

---
+++ Prog1/Prog1.ino
@@ -0,0 +1,74 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //T
+  ratlla();
+  espaiL();
+  //U
+  punt();
+  punt();
+  ratlla();
+  espaiP();
+  //P
+  punt();
+  ratlla();
+  ratlla();
+  punt();
+  espaiL();
+  //O
+  ratlla();
+  ratlla();
+  ratlla();
+  //Espai entre paraules
+  espaiL();
+  //S
+  punt();
+  punt();
+  punt();
+    
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
